7c837b1750d11770b706ba49b94a0c6aa87af243
turkworks <andrewcengiz@gmail.com>
2019-01-31 20:35:33 -0700

    Display and Dashboard

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,25 @@
 // Test away!
+import React from "react";
+import { render } from "react-testing-library";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("should use red-led class when locked", () => {
+    const { getByText } = render(<Display locked={true} />);
+
+    const lockedDisplay = getByText(/locked/i);
+
+    expect(lockedDisplay).toHaveClass("red-led");
+  });
+
+  it("should use green-led class when unlocked", () => {
+    const { getByText } = render(<Display locked={false} />);
+
+    const lockedDisplay = getByText(/unlocked/i);
+
+    expect(lockedDisplay).toHaveClass("green-led");
+  });
+});
